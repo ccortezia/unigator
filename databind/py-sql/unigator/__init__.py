679d2055c2f0d47498b36d8d873a0ab0005a2be2
Cristiano Cortezia <cristiano@britecore.com>
2019-12-29 16:34:39 -0300

    Remove databind layer

--- databind/py-sql/unigator/__init__.py
+++
@@ -1,4 +0,0 @@
-# flake8: noqa
-from .databind import UnigatorDataBind
-from .exceptions import UnigatorDataBindError, UnigatorDataBindUnknownQuery
-
--- databind/py-sql/unigator/exceptions.py
+++
@@ -1,7 +0,0 @@
-
-class UnigatorDataBindError(Exception):
-    """Base exception class for the unigator datasources"""
-
-
-class UnigatorDataBindUnknownQuery(UnigatorDataBindError):
-    """Raised when an unknown query is requested"""
