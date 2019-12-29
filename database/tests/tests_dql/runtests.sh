f7945697ca20f3333ca33d2af14849683e836d08
Cristiano Cortezia <cristiano@britecore.com>
2019-12-29 16:35:12 -0300

    Remove database/tests prior to replacement

--- database/tests/tests_dql/runtests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -u
-
-readonly HERE="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
-readonly DSPATH=$HERE/../../../databind/py-sql/
-
-PGUSER=unigator PGHOST=localhost PYTHONPATH=$DSPATH py.test -s $@
--- database/tests/tests_dql/utils.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-
-def dt(y, mo, d, h=0, m=0):
-    """Datetime creator, alias"""
-    return datetime.datetime(y, mo, d, h, m)
